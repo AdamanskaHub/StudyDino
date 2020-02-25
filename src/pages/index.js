8eae37a66b54d15e407ed9402e78c6917a0e2377
Emilie <adamanska@gmail.com>
2020-02-25 11:33:36 +0100

    included all the html elements and gen structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,56 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+// import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 import "../pages/styles.scss"
+import dino from "../images/dino.png"
 
 const IndexPage = () => (
-  <Layout>
-    <div class="bg-container"> 
+  // <Layout>
+  <div class="container">
+    <div className="marginer">
+
     <SEO title="Study Dino" />
-    <p>Hi Username</p>
-    <a href="" class="eightbit-btn">START</a>
-    <div class="bottombar">
-      <Link to="/inventory/">inventory-icon</Link>
-    </div>
+
+<div class="top-bar">
+  <Link to="/inventory/">inventory-icon</Link>
+  <Link to="/shop/">shop-icon</Link>
+  <Link to="/settings/">settings-icon</Link>
+</div>
+<span class="coins">30</span>
+<p class="greetings">Hi DynamicallyChanged</p>
+
+<div className="start-grp">
+
+  <div className="start-grp__top">
+    <button className="time-adjust minus">-</button>
+    <input type="time" className="time-input" min="00:10" max="08:00" required  />
+    <button className="time-adjust plus">+</button>
+  </div>
+
+  <a href="" class="eightbit-btn">START</a>
+
+</div>
+
+<div className="img-container">
+  <img class="start-dino" src={dino} alt="dino" />
+</div>
+
+
     </div>
     
-    
-  </Layout>
+
+
+
+
+  </div>
+
+
+
+  // </Layout>
 )
 
 export default IndexPage
